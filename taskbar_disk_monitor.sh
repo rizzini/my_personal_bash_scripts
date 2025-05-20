09af1d1200c9bbdf9ccd6c31d5e22fe906d2033d
Lucas Rizzini <lucasrizzini@hotmail.com>
2025-05-19 21:21:53 -0300

    Add unit_mode variable for KB/s, MB/s or auto unit selection

--- taskbar_disk_monitor.sh
+++ taskbar_disk_monitor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+unit_mode=2
 declare -A data_read1 data_write1 read_total write_total counter time_alert_read time_alert_write
 declare -a disk_list=()
 contains() {
@@ -46,28 +47,59 @@ collect_data() {
 }
 display_data() {
     local output=""
+    local use_mb read_kb write_kb read_integer read_fraction write_integer write_fraction
     if [ "$1" == 'all' ]; then
         for disk in "${disk_list[@]}"; do
-            if [[ ${read_total[$disk]} -ge 1024 ]]; then
-                read_kb=$((read_total[$disk] * 100 / 1024))
-                read_integer=$((read_kb / 100))
-                read_fraction=$(printf "%02d" $((read_kb % 100)))
-                value_read="${read_integer}.${read_fraction}"
-                unit_read="MB/s"
-            else
-                value_read=${read_total[$disk]}
-                unit_read="KB/s"
-            fi
-            if [[ ${write_total[$disk]} -ge 1024 ]]; then
-                write_kb=$((write_total[$disk] * 100 / 1024))
-                write_integer=$((write_kb / 100))
-                write_fraction=$(printf "%02d" $((write_kb % 100)))
-                value_write="${write_integer}.${write_fraction}"
-                unit_write="MB/s"
-            else
-                value_write=${write_total[$disk]}
-                unit_write="KB/s"
-            fi
+            case "$unit_mode" in
+                1)
+                    value_read=${read_total[$disk]}
+                    unit_read="KB/s"
+                    ;;
+                2)
+                    read_kb=$((read_total[$disk] * 100 / 1024))
+                    read_integer=$((read_kb / 100))
+                    read_fraction=$(printf "%02d" $((read_kb % 100)))
+                    value_read="${read_integer}.${read_fraction}"
+                    unit_read="MB/s"
+                    ;;
+                3)
+                    if [[ ${read_total[$disk]} -ge 1024 ]]; then
+                        read_kb=$((read_total[$disk] * 100 / 1024))
+                        read_integer=$((read_kb / 100))
+                        read_fraction=$(printf "%02d" $((read_kb % 100)))
+                        value_read="${read_integer}.${read_fraction}"
+                        unit_read="MB/s"
+                    else
+                        value_read=${read_total[$disk]}
+                        unit_read="KB/s"
+                    fi
+                    ;;
+            esac
+            case "$unit_mode" in
+                1)
+                    value_write=${write_total[$disk]}
+                    unit_write="KB/s"
+                    ;;
+                2)
+                    write_kb=$((write_total[$disk] * 100 / 1024))
+                    write_integer=$((write_kb / 100))
+                    write_fraction=$(printf "%02d" $((write_kb % 100)))
+                    value_write="${write_integer}.${write_fraction}"
+                    unit_write="MB/s"
+                    ;;
+                3)
+                    if [[ ${write_total[$disk]} -ge 1024 ]]; then
+                        write_kb=$((write_total[$disk] * 100 / 1024))
+                        write_integer=$((write_kb / 100))
+                        write_fraction=$(printf "%02d" $((write_kb % 100)))
+                        value_write="${write_integer}.${write_fraction}"
+                        unit_write="MB/s"
+                    else
+                        value_write=${write_total[$disk]}
+                        unit_write="KB/s"
+                    fi
+                    ;;
+            esac
             current_time=$(date +%s)
             if [[ -n "${time_alert_read[$disk]}" && $((current_time - time_alert_read[$disk])) -le 3 ]]; then
                 value_read="\e[91m${value_read}${unit_read}\e[0m"
@@ -87,26 +119,58 @@ display_data() {
         if [[ ! " ${disk_list[@]} " =~ " ${specified_disk} " ]]; then
             output="Error: Device '${specified_disk}' is not present in the system."
         else
-            if [[ ${read_total[$specified_disk]} -ge 1024 ]]; then
-                read_kb=$((read_total[$specified_disk] * 100 / 1024))
-                read_integer=$((read_kb / 100))
-                read_fraction=$(printf "%02d" $((read_kb % 100)))
-                value_read="${read_integer}.${read_fraction}"
-                unit_read="MB/s"
-            else
-                value_read=${read_total[$specified_disk]}
-                unit_read="KB/s"
-            fi
-            if [[ ${write_total[$specified_disk]} -ge 1024 ]]; then
-                write_kb=$((write_total[$specified_disk] * 100 / 1024))
-                write_integer=$((write_kb / 100))
-                write_fraction=$(printf "%02d" $((write_kb % 100)))
-                value_write="${write_integer}.${write_fraction}"
-                unit_write="MB/s"
-            else
-                value_write=${write_total[$specified_disk]}
-                unit_write="KB/s"
-            fi
+            case "$unit_mode" in
+                1)
+                    value_read=${read_total[$specified_disk]}
+                    unit_read="KB/s"
+                    ;;
+                2)
+                    read_kb=$((read_total[$specified_disk] * 100 / 1024))
+                    read_integer=$((read_kb / 100))
+                    read_fraction=$(printf "%02d" $((read_kb % 100)))
+                    value_read="${read_integer}.${read_fraction}"
+                    unit_read="MB/s"
+                    ;;
+                3)
+                    if [[ ${read_total[$specified_disk]} -ge 1024 ]]; then
+                        read_kb=$((read_total[$specified_disk] * 100 / 1024))
+                        read_integer=$((read_kb / 100))
+                        read_fraction=$(printf "%02d" $((read_kb % 100)))
+                        value_read="${read_integer}.${read_fraction}"
+                        unit_read="MB/s"
+                    else
+                        value_read=${read_total[$specified_disk]}
+                        unit_read="KB/s"
+                    fi
+                    ;;
+            esac
+
+            case "$unit_mode" in
+                1)
+                    value_write=${write_total[$specified_disk]}
+                    unit_write="KB/s"
+                    ;;
+                2)
+                    write_kb=$((write_total[$specified_disk] * 100 / 1024))
+                    write_integer=$((write_kb / 100))
+                    write_fraction=$(printf "%02d" $((write_kb % 100)))
+                    value_write="${write_integer}.${write_fraction}"
+                    unit_write="MB/s"
+                    ;;
+                3)
+                    if [[ ${write_total[$specified_disk]} -ge 1024 ]]; then
+                        write_kb=$((write_total[$specified_disk] * 100 / 1024))
+                        write_integer=$((write_kb / 100))
+                        write_fraction=$(printf "%02d" $((write_kb % 100)))
+                        value_write="${write_integer}.${write_fraction}"
+                        unit_write="MB/s"
+                    else
+                        value_write=${write_total[$specified_disk]}
+                        unit_write="KB/s"
+                    fi
+                    ;;
+            esac
+
             current_time=$(date +%s)
             if [[ -n "${time_alert_read[$specified_disk]}" && $((current_time - time_alert_read[$specified_disk])) -le 3 ]]; then
                 value_read="\e[91m${value_read}${unit_read}\e[0m"
