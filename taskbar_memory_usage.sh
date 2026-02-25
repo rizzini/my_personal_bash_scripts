69431bc95076514ae9c4feb3bd4ad44450833200
Lucas Rizzini <lucasrizzini@hotmail.com>
2026-02-25 07:53:17 -0300

    atualizando..

--- taskbar_memory_usage.sh
+++ taskbar_memory_usage.sh
@@ -7,51 +7,23 @@ enable_zram() {
         modprobe zram num_devices=1 max_comp_streams=4
     fi
     echo zstd > /sys/block/zram0/comp_algorithm
-    echo 6G > /sys/block/zram0/disksize
+    echo 4G > /sys/block/zram0/disksize
     mkswap -U clear /dev/zram0
     swapon --discard --priority 100 /dev/zram0
-    sysctl -w vm.watermark_boost_factor=0 vm.watermark_scale_factor=125 vm.page-cluster=0 vm.swappiness=180
-    swapon --priority 10 /dev/sda8
-    sysctl vm.swappiness=10
+    swapon --priority 1 /dev/sda8
+    sysctl -w vm.watermark_boost_factor=0 vm.watermark_scale_factor=100 vm.page-cluster=0 vm.swappiness=10 vm.vfs_cache_pressure=50
 }
 disable_zram() {
-    can_disable_zram
-    if ! (( mem_available_kb + other_swap_free_kb >= zram_used_kb )); then
-        notify-send -u critical "Refusing to disable zram" "Not enough memory/swap to relocate ${zram_used_kb} KB from zram.\nMemAvailable=${mem_available_kb} KB, other swap free=${other_swap_free_kb} KB"
-        return 1
-    fi
-
     for s in $(awk 'NR>1 {print $1}' /proc/swaps); do
         swapoff "$s" || { notify-send -u critical "Failed to swapoff $s"; return 1; }
     done
-
+    sysctl -w vm.watermark_boost_factor=15000 vm.watermark_scale_factor=10 vm.page-cluster=3 vm.swappiness=60 vm.vfs_cache_pressure=100
     echo 1 > /sys/module/zswap/parameters/enabled
     while ! modprobe -r zram; do
-        sleep 1;
+        sleep 0.5;
     done
 }
 
-
-can_disable_zram() {
-    mem_available_kb=$(awk '/MemAvailable:/ {print $2}' /proc/meminfo)
-    if [ -z "$mem_available_kb" ]; then
-        mem_free=$(awk '/MemFree:/ {print $2}' /proc/meminfo)
-        buffers=$(awk '/Buffers:/ {print $2}' /proc/meminfo)
-        cached=$(awk '/Cached:/ {print $2}' /proc/meminfo)
-        sreclaimable=$(awk '/SReclaimable:/ {print $2}' /proc/meminfo)
-        mem_available_kb=$((mem_free + buffers + cached + sreclaimable))
-    fi
-
-    other_swap_free_kb=0
-    zram_used_kb=0
-    while read -r name type size used pr; do
-        if [[ "$name" == /dev/zram* ]]; then
-            zram_used_kb=$((zram_used_kb + used))
-        else
-            other_swap_free_kb=$((other_swap_free_kb + (size - used)))
-        fi
-    done < <(tail -n +2 /proc/swaps)
-}
 if [ "$1" == 'click' ]; then
     if [[ "$swaps" != *"/dev/zram0"* ]]; then
         enable_zram
